772c87718491ef4ef70c14043eda84b6a82a3899
Olivia Dang <78009350+oliviadangpad@users.noreply.github.com>
2023-10-27 18:25:50 +1100

    Create form

---
+++ src/form/form.vue
@@ -0,0 +1,70 @@
+<!-- 
+    This is a basic form created based on https://digitalpatio.hashnode.dev/build-better-forms-with-vuejs-3-composition-api-a-practical-guide
+    It looks like this 
+
+-->
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h2>{{ props.foo }}</h2>
+    <label>
+      First Name:
+      <input type="text" v-model="formData.firstName" placeholder="First Name" />
+      <div v-if="!isValidFirstName" class="error">Please enter a valid first name</div>
+
+    </label>
+    <label>
+      Last Name:
+      <input type="text" v-model="formData.lastName" placeholder="Last Name" />
+      <div v-if="!isValidLastName" class="error">Please enter a valid last name</div>
+
+    </label>
+
+    <br />
+    <div v-if="errorMessage">{{ errorMessage }}</div>
+    <button type="submit" :disabled="!isFormValid">Submit</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { reactive, computed, ref } from 'vue';
+
+interface Props {
+  foo: string
+  bar?: number
+}
+
+const props = defineProps<Props>()
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+}
+
+// Define reactive state for form data
+const formData = reactive<FormData>({ firstName: '', lastName: '' })
+
+// Define computed properties for form validation
+const isValidFirstName = computed(() => formData.firstName);
+const isValidLastName = computed(() => formData.lastName);
+const isFormValid = computed(() => isValidFirstName.value && isValidLastName.value);
+
+
+// error message
+const errorMessage = ref('');
+
+const handleSubmit = () => {
+  try {
+    onSubmit(formData);
+  } catch (error) {
+    errorMessage.value = 'An error occurred while submitting the form';
+    console.error(error);
+  }
+
+};
+
+const onSubmit = ({ firstName, lastName }) => {
+  // Perform form submission logic here
+  alert(JSON.stringify(firstName, lastName))
+}
+</script>
